4e55a18e9fa638b2f40d29017811c35d1e394dae
Nusferaty <94083597+Nusferaty@users.noreply.github.com>
2021-12-09 23:56:12 +0300

    пр10

---
+++ Практическая работа 10/src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+// Класс Ping.
+class Ping implements Runnable{
+    @Override
+    public void run() {
+        while (true) {
+            // Выводим "Ping".
+            System.out.println("Ping");
+            try {
+                // Делаем задержку потока на 250 мс.
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+//
+class Pong implements Runnable{
+    @Override
+    public void run() {
+
+         while (true) {
+            // Выводим "Pong".
+            System.out.println("Pong");
+            try {
+                // Делаем задержку потока на 250 мс.
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args)
+    {
+        // Запускаем "Ping" в отдельном потоке.
+        Ping ping = new Ping();
+        Thread pingObj = new Thread(ping);
+        pingObj.start();
+
+        // Запускаем "Pong" в отдельном потоке.
+        Pong pong = new Pong();
+        Thread pongObj = new Thread(pong);
+        pongObj.start();
+    }
+}
